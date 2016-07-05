dcf16dd1ed09c9a38d1e2626c63d765e0474c9b6
admin <admin@DEV-PC0>
2016-07-05 13:35:48 +0800

    t

---
+++ Source/Applications/XKNT.UI.PC.Controllers/Controllers/Home/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using XKNT.Common.Helper;
+
+namespace XKNT.UI.PC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult index()
+        {
+            ArrayList al = new ArrayList();
+
+            al.Add("127.0.0.1:11211");
+
+            bool result = MemcacheHelper.AddCache(al, "mc", "test", "hello word!", 1);
+
+            return Content("test的缓存内容为：" + MemcacheHelper.GetCache(al, "mc", "test").ToString());
+        }
+    }
+}
